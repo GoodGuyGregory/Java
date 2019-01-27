5d8bbb348375417e090be782998712229a0097c8
GoodGuyGregory <greg.witt625@gmail.com>
2019-01-27 18:11:50 -0500

    completes code work for modules on Arrays

--- Java_Arrays/scratch.java
+++ Java_Arrays/scratch.java
@@ -1,10 +1,37 @@
 /*Jshell: /open scratch.java
   to utilize this datasctructure in terminal*/
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 // int[][] scoreCards = {
-//   {1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
-//   {2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
-//   {4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
-// }
 
-String[] friends = {"Greg", "Jon", "Matt"};
+String[] friends = {
+                    "Greg",
+                    "Ryan",
+                    "Xiao",
+                    "Jake"};
+
+// Arrays.sort(<array>);
+//Arrays.sort(friends, Comparator.comparing(String::length));
+//
+// Assume that you have a class representing Planet. It has a method named getDistanceInAstromicalUnits that helps figure out the distance from the Sun.
+//
+// Instances of the Planet class are created and placed in an Planet[] array named planets.
+//
+// Which code snippet would allow you to sort the planets array by distance?
+
+// Arrays.sort(planets, Comparator.comparing(Planet::getDistanceInAstromicalUnits));
+
+//method call in picklunch spots is Varargs
+public String pickLunchSpot(String... spots){
+  System.out.printf("Randomly Picking %d Lunch spots %n",
+                        spots.length);
+  if(spots.length == 0)
+    return "Enter a place my dude";
+
+  //number starts at zero in Random..
+  Random random = new Random();
+  return spots[random.nextInt(spots.length)];
+
+}
