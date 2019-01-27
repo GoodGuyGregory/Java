0cdb5b0b1194776a1b901f397b4e8fee3c098801
GoodGuyGregory <greg.witt625@gmail.com>
2019-01-27 17:26:25 -0500

    adds files

--- Java_Arrays/Explore.java
+++ Java_Arrays/Explore.java
@@ -3,23 +3,50 @@ public class Explore {
   public static void main(String[] args) {
     //Creates a new Array on a single
     //Includes friends: Nick, Zack, Nathan, Ryan
-    String[] friends = {"Jake", "Tyler", "Zack", "Ryan", "Nick"};
-    //array iterator through the friends Array
-    //Enhanced for loop
-    for(String friend : friends){
-      System.out.printf("Hey %s! The movie starts at 7pm, See you there! %n",friend);
-    }//for
+    //String[] friends = {"Jake", "Tyler", "Zack", "Ryan", "Nick"};
+    /*Jshell: /open scratch.java
+      to utilize this datasctructure in terminal*/
+    //Creates a New String Array with Three friends for the t@D array to fit
+    String[] friends = {"Greg", "Jon", "Matt"};
 
-    for (int i = 0;i < friends.length; i++ ) {
-      String friend = friends[i];
-      System.out.printf("Hey %s! The movie starts at 7pm, See you there! %n", friend);
-    }//for
+    int[][] scoreCards = {
+        {1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
+        {2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
+        {4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+      };
 
-    int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
-    for (int i = 0;i < bensScoreCard.length;i++ ) {
-      System.out.printf("Hole #%d: %d %n",
-                          i + 1,
-                          bensScoreCard[i]);
-    }//for
-  }//main
+      // for each friend loop thorough
+      for (int i = 0;i < friends.length;i++ ) {
+     // print their name
+        System.out.printf("%s %n-------------------------%n",
+                          friends[i]);
+          // for each Hole
+          for (int j = 0; j < scoreCards[i].length; j++ ) {
+            //print score
+            System.out.printf("Hole: #%d: %d %n",
+                              j+1,
+                              scoreCards[i][j]);
+          }//for
+        }//for
+      }//main
 }//class
+
+
+    // //array iterator through the friends Array
+    // //Enhanced for loop
+    // for(String friend : friends){
+    //   System.out.printf("Hey %s! The movie starts at 7pm, See you there! %n",friend);
+    // }//for
+    //
+    // for (int i = 0;i < friends.length; i++ ) {
+    //   String friend = friends[i];
+    //   System.out.printf("Hey %s! The movie starts at 7pm, See you there! %n", friend);
+    // }//for
+    //
+    // int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
+    // for (int i = 0;i < bensScoreCard.length;i++ ) {
+    //   System.out.printf("Hole #%d: %d %n",
+    //                       i + 1,
+    //                     bensScoreCard[i]);
+  //  }//for
+//}//class
---
+++ Java_Arrays/boggle.java
@@ -0,0 +1,19 @@
+public class boggle{
+
+public static void main(String[] args) {
+  char[][] boggle = {
+          {'C', 'A', 'T'},
+          {'D', 'R', 'I'},
+          {'L', 'O', 'G'}
+  };
+
+  System.out.printf("-------------%n");
+  for (int i = 0; i < boggle.length; i++) {
+    for (int j = 0; j < boggle[i].length; j++) {
+      System.out.printf("| %s ", boggle[i][j]);
+     }
+     System.out.printf("|%n-------------%n");
+  }
+
+  }
+}
